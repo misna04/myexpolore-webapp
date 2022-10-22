502f7b606ff0c057afcaa656a7c1fb3f703fce2d
misna04 <misnasyari@gmail.com>
2022-10-22 11:09:44 +0800

    config react router onprogress

---
+++ src/router/Pages.js
@@ -0,0 +1,22 @@
+const PagesRoutes = [
+    {
+        path: "/form",
+        component: Sandwiches
+    },
+    {
+        path: "/tacos",
+        component: Tacos,
+        routes: [
+            {
+                path: "/tacos/bus",
+                component: Bus
+            },
+            {
+                path: "/tacos/cart",
+                component: Cart
+            }
+        ]
+    }
+]
+
+export default PagesRoutes
