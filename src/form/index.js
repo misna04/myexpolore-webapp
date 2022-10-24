3a53619169c10eabc74c964bb31967a67e54ef59
misna04 <misnasyari@gmail.com>
2022-10-24 12:30:42 +0800

    autocomplete

--- src/form/index.js
+++ src/form/index.js
@@ -1,11 +1,14 @@
 import * as React from "react"
-import TextField from "@mui/material/TextField"
-import Dialog from "@mui/material/Dialog"
-import DialogTitle from "@mui/material/DialogTitle"
-import DialogContent from "@mui/material/DialogContent"
-import DialogContentText from "@mui/material/DialogContentText"
-import DialogActions from "@mui/material/DialogActions"
-import Button from "@mui/material/Button"
+import {
+    TextField,
+    Dialog,
+    DialogTitle,
+    DialogContent,
+    DialogContentText,
+    DialogActions,
+    Button,
+    Box
+} from "@mui/material"
 import Autocomplete, { createFilterOptions } from "@mui/material/Autocomplete"
 
 const filter = createFilterOptions()
@@ -40,64 +43,66 @@ export default function FreeSoloCreateOptionDialog() {
 
     return (
         <React.Fragment>
-            <Autocomplete
-                value={value}
-                onChange={(event, newValue) => {
-                    if (typeof newValue === "string") {
-                        // timeout to avoid instant validation of the dialog's form.
-                        setTimeout(() => {
+            <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+                <Autocomplete
+                    value={value}
+                    onChange={(event, newValue) => {
+                        if (typeof newValue === "string") {
+                            // timeout to avoid instant validation of the dialog's form.
+                            setTimeout(() => {
+                                toggleOpen(true)
+                                setDialogValue({
+                                    title: newValue,
+                                    year: ""
+                                })
+                            })
+                        } else if (newValue && newValue.inputValue) {
                             toggleOpen(true)
                             setDialogValue({
-                                title: newValue,
+                                title: newValue.inputValue,
                                 year: ""
                             })
-                        })
-                    } else if (newValue && newValue.inputValue) {
-                        toggleOpen(true)
-                        setDialogValue({
-                            title: newValue.inputValue,
-                            year: ""
-                        })
-                    } else {
-                        setValue(newValue)
-                    }
-                }}
-                filterOptions={(options, params) => {
-                    const filtered = filter(options, params)
+                        } else {
+                            setValue(newValue)
+                        }
+                    }}
+                    filterOptions={(options, params) => {
+                        const filtered = filter(options, params)
 
-                    if (params.inputValue !== "") {
-                        filtered.push({
-                            inputValue: params.inputValue,
-                            title: `Add "${params.inputValue}"`
-                        })
-                    }
+                        if (params.inputValue !== "") {
+                            filtered.push({
+                                inputValue: params.inputValue,
+                                title: `Add "${params.inputValue}"`
+                            })
+                        }
 
-                    return filtered
-                }}
-                id="free-solo-dialog-demo"
-                options={top100Films}
-                getOptionLabel={(option) => {
-                    // e.g value selected with enter, right from the input
-                    if (typeof option === "string") {
-                        return option
-                    }
-                    if (option.inputValue) {
-                        return option.inputValue
-                    }
-                    return option.title
-                }}
-                selectOnFocus
-                clearOnBlur
-                handleHomeEndKeys
-                renderOption={(props, option) => (
-                    <li {...props}>{option.title}</li>
-                )}
-                sx={{ width: 300 }}
-                freeSolo
-                renderInput={(params) => (
-                    <TextField {...params} label="Free solo dialog" />
-                )}
-            />
+                        return filtered
+                    }}
+                    id="free-solo-dialog-demo"
+                    options={top100Films}
+                    getOptionLabel={(option) => {
+                        // e.g value selected with enter, right from the input
+                        if (typeof option === "string") {
+                            return option
+                        }
+                        if (option.inputValue) {
+                            return option.inputValue
+                        }
+                        return option.title
+                    }}
+                    selectOnFocus
+                    clearOnBlur
+                    handleHomeEndKeys
+                    renderOption={(props, option) => (
+                        <li {...props}>{option.title}</li>
+                    )}
+                    sx={{ width: 300 }}
+                    freeSolo
+                    renderInput={(params) => (
+                        <TextField {...params} label="Free solo dialog" />
+                    )}
+                />
+            </Box>
             <Dialog open={open} onClose={handleClose}>
                 <form onSubmit={handleSubmit}>
                     <DialogTitle>Add a new film</DialogTitle>
